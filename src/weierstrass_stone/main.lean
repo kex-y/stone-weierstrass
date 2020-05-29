0e912892ecab56a3b18aa18db211085ed8b06eae
Kexing <kexing.ying19@imperial.ac.uk>
2020-05-29 21:39:08 +0200

    done `eq_iff_boundary_points_eq` and its lemmas

--- src/weierstrass_stone/main.lean
+++ src/weierstrass_stone/main.lean
@@ -6,8 +6,8 @@ open set
 
 variables {X : Type*} [metric_space X] [compact_space X]
 
-/- We have some API regarding bdd continuous functions in mathlib! -/
-#check bounded_continuous_function X ℝ
+/- We have some API regarding bdd continuous functions in mathlib! 
+#check bounded_continuous_function X ℝ -/
 
 theorem max_bounded {f g : X → ℝ} 
 (hf :  ∃ (C : ℝ), ∀ (x y : X), dist (f x) (f y) ≤ C) 
@@ -47,6 +47,12 @@ def closure₂ (M₀ : set (bounded_continuous_function X ℝ)) :=
   (∃ f g ∈ M₀, h = f ⊔ g ∨ h = f ⊓ g) ∨ 
   ∃ (F : ℕ → (bounded_continuous_function X ℝ)) 
     (H : ∀ i, F i ∈ closure₀ M₀), unif_converges_to (λ n, F n) h}
+
+lemma closure₀_closed_with_sup {M₀ : set (bounded_continuous_function X ℝ)}
+{f g} (hf : f ∈ closure₀ M₀) (hg : g ∈ closure₀ M₀) : f ⊔ g ∈ closure₀ M₀ := sorry
+
+lemma closure₀_closed_with_inf {M₀ : set (bounded_continuous_function X ℝ)}
+{f g} (hf : f ∈ closure₀ M₀) (hg : g ∈ closure₀ M₀) : f ⊓ g ∈ closure₀ M₀ := sorry
 
 attribute [reducible] closure₀ closure₁ closure₂
 
@@ -94,8 +100,6 @@ end
 Since X is a compact space, We an use Heine-Borel on the entire space.
 
 This is the lemma we'll use: compact.elim_finite_subcover -/
-
-#check compact.elim_finite_subcover
 
 /-
 So let us fix x ∈ X and given y ∈ X write the function g satisfying 
@@ -222,9 +226,138 @@ begin
   norm_cast, exact le_add_right hn,
 end
 
+/-
+Finally, we can conclude that f can be constructed from M₀ using inf, sup 
+and uniform convergence iff. for all pairs of points in X, (x, y), there is 
+some sequence of functions in M₀ that converges pointwise to f(x) and f(y) at 
+x and y.
+-/
 theorem in_closure₂_iff_dense_at_points 
 {f : bounded_continuous_function X ℝ} : 
 f ∈ closure₂ M₀ ↔ ∀ ε > 0, ∀ x y : X,
 ∃ (g : bounded_continuous_function X ℝ) (H : g ∈ closure₀ M₀), 
 abs (f x - g x) < ε ∧ abs (f y - g y) < ε := 
 ⟨λ h, dense_at_points_in_closure h, λ h, in_closure₂_of_dense_at_points h⟩
+
+/-
+From this, two corollaries can be deduced immediately:
+
+- If ∀ x, y ∈ X, a, b ∈ ℝ, ∃ f ∈ M₀, f(x) = a, f(y) = b, then 
+  closure₂ M₀ = M (= @univ bounded_continuous_function X ℝ)
+where M is the set of all bounded continuous functions from X to ℝ.
+
+- If f : ℕ → bounded_continuous_function X ℝ is monotone and converges 
+pointwise to g, then f converges uniformly to g.
+
+The proof of the first corollary is trivial and the proof of the second 
+follows by letting M₀ := {f n | n ∈ ℕ}.
+-/
+
+/-
+Due to reasons commented from above, we will now consider pairs of points in 
+X rather than functions. 
+
+We define M₀' : X → X → ℝ × ℝ : x, y ↦ {(f(x), f(y)) | f ∈ M₀};
+    (α, β) ⊔ (γ, δ) := (max α γ, max β δ);
+and (α, β) ⊓ (γ, δ) := (min α γ, min β δ).
+
+We also define a closure with respect to this inf, sup and limits of 
+sequences in ℝ². 
+-/
+
+def boundary_points (M : set (bounded_continuous_function X ℝ)) (x y : X) := 
+  {z | ∃ (f : bounded_continuous_function X ℝ) (hf : f ∈ closure₀ M), (f x, f y) = z}
+
+-- We do not need to define inf and sup for tuples since matlib has it!
+-- try : #eval (1, 4) ⊔ (2, 3)
+
+-- Limit of sequences in ℝ²
+def converges_to_R2 (s : ℕ → ℝ × ℝ) (x) := 
+  ∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, dist (s n) x < ε
+
+def closure' (S : set (ℝ × ℝ)) := 
+  {z | (∃ r t ∈ S, r ⊔ t = z ∨ r ⊓ t = z) ∨ 
+  ∃ (s : ℕ → ℝ × ℝ) (hs : ∀ n : ℕ, (s n) ∈ S), converges_to_R2 s z}
+
+variables (a b : ℝ × ℝ)
+
+attribute [reducible] boundary_points closure'
+
+lemma in_closure'_of_in_closoure₂ {f : bounded_continuous_function X ℝ} 
+(h : f ∈ closure₂ M₀) : ∀ x y : X, (f x, f y) ∈ closure' (boundary_points M₀ x y) :=
+begin
+  intros x y, right,
+  rw in_closure₂_iff_dense_at_points at h,
+  choose F hF₀ hF₁ using λ (n : ℕ), 
+    h (1 / (n + 1)) (nat.one_div_pos_of_nat) x y,
+  refine ⟨λ n, (F n x, F n y), λ n, ⟨F n, hF₀ n, rfl⟩, λ ε hε, _⟩, 
+  cases exists_nat_gt (1 / ε) with N hN,
+  refine ⟨N, λ n hn, _⟩,
+  suffices : abs (F n x - f x) < ε ∧ abs (F n y - f y) < ε,
+    unfold dist, simp [this],
+  split; rw abs_sub;
+    try { refine lt_of_lt_of_le (hF₁ n).1 _ <|> refine lt_of_lt_of_le (hF₁ n).2 _ };
+    refine one_div_le_of_one_div_le_of_pos hε (le_trans (le_of_lt hN) _);
+    norm_cast; exact le_add_right hn
+end
+
+lemma bcf_pair_sup_eq_bcf_sup_pair {u v : bounded_continuous_function X ℝ} {x y} : 
+(u x, u y) ⊔ (v x, v y) = ((u ⊔ v) x, (u ⊔ v) y) := rfl
+
+lemma bcf_pair_inf_eq_bcf_inf_pair {u v : bounded_continuous_function X ℝ} {x y} : 
+(u x, u y) ⊓ (v x, v y) = ((u ⊓ v) x, (u ⊓ v) y) := rfl
+
+lemma in_closure₂_of_in_closure' {f : bounded_continuous_function X ℝ} 
+(h : ∀ x y : X, (f x, f y) ∈ closure' (boundary_points M₀ x y)) : f ∈ closure₂ M₀ :=
+begin
+  rw in_closure₂_iff_dense_at_points,
+  intros ε hε x y,
+  cases h x y with h' h',
+    { rcases h' with ⟨r, t, ⟨u, hu₀, hu₁⟩, ⟨v, hv₀, hv₁⟩, hrt⟩,
+      cases hrt,
+        { refine ⟨u ⊔ v, closure₀_closed_with_sup hu₀ hv₀, _⟩,
+          rw [←hu₁, ←hv₁, bcf_pair_sup_eq_bcf_sup_pair] at hrt,
+          rw [(prod.mk.inj hrt).1, (prod.mk.inj hrt).2], simpa },
+        { refine ⟨u ⊓ v, closure₀_closed_with_inf hu₀ hv₀, _⟩,
+          rw [←hu₁, ←hv₁, bcf_pair_inf_eq_bcf_inf_pair] at hrt,
+          rw [(prod.mk.inj hrt).1, (prod.mk.inj hrt).2], simpa } },
+    { rcases h' with ⟨s, hs₀, hs₁⟩,
+      cases hs₁ ε hε with N hN, 
+      rcases hs₀ N with ⟨g, hg₀, hg₁⟩,
+      refine ⟨g, hg₀, _⟩,
+      have := hN N (le_refl N),
+      rw ←hg₁ at this, unfold dist at this, simp at this,
+      split; rw abs_sub; simp only [this] }
+end
+
+lemma in_closure₂_iff_in_closure' {f : bounded_continuous_function X ℝ} : 
+f ∈ closure₂ M₀ ↔ ∀ x y : X, (f x, f y) ∈ closure' (boundary_points M₀ x y) :=
+⟨λ h, in_closure'_of_in_closoure₂ h, λ h, in_closure₂_of_in_closure' h⟩
+
+/- 
+With this, we can formulate the relation between M₀ and M₀'(x, y) formally, i.e.
+if M₀, M₁ ⊆ M, 
+  hM : M₀ = closure₂ M₀ and M₁ = closure₂ M₁
+  hb : ∀ x, y ∈ X, M₀*(x, y) = M₁*(x, y)
+then M₀ = M₁.
+-/
+lemma boundary_points_eq_of_eq (M₀ M₁ : set (bounded_continuous_function X ℝ))
+(h : M₀ = M₁) : ∀ x y : X, boundary_points M₀ x y = boundary_points M₁ x y := 
+λ x y, by rw h
+
+lemma eq_of_boundary_points_eq (M₀ M₁ : set (bounded_continuous_function X ℝ))
+(h : ∀ x y : X, boundary_points M₀ x y = boundary_points M₁ x y)
+(hM₀ : M₀ = closure₂ M₀) (hM₁ : M₁ = closure₂ M₁) : M₀ = M₁ := 
+begin
+  ext f, split; 
+  { intro hf,
+    try {rw hM₀ <|> rw hM₁}, try {rw hM₀ at hf <|> rw hM₁ at hf},
+    rw in_closure₂_iff_in_closure' at *, 
+    intros x y, try {rw ←h x y <|> rw h x y},
+    exact hf x y }
+end
+
+theorem eq_iff_boundary_points_eq (M₀ M₁ : set (bounded_continuous_function X ℝ))
+(hM₀ : M₀ = closure₂ M₀) (hM₁ : M₁ = closure₂ M₁) : 
+M₀ = M₁ ↔ ∀ x y : X, boundary_points M₀ x y = boundary_points M₁ x y := 
+⟨λ h, boundary_points_eq_of_eq M₀ M₁ h, λ h, eq_of_boundary_points_eq M₀ M₁ h hM₀ hM₁⟩
